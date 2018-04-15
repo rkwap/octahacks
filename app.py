cbdc3fdf6135edfa9b46c6cef313b8e4d08152c7
Ramit <30972307+rkwap@users.noreply.github.com>
2018-04-15 11:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ def cal_distime():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(host = "0.0.0.0",debug=True, port=8080)
+    app.run(host = "0.0.0.0",debug=True)
